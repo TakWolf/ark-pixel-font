3e97e29659124e72fe98f5c4ac0e186c6c63891c
TakWolf <takwolf@foxmail.com>
2021-05-11 23:01:58 +0800

    fs 工具类添加简单创建目录的方法

--- utils/fs_util.py
+++ utils/fs_util.py
@@ -9,3 +9,11 @@ def cleanup_dir(dir_path):
     if os.path.exists(dir_path):
         shutil.rmtree(dir_path)
     os.makedirs(dir_path)
+
+
+def make_dirs_if_not_exists(dir_path):
+    """
+    如果文件夹不存在，则创建文件夹
+    """
+    if not os.path.exists(dir_path):
+        os.makedirs(dir_path)
