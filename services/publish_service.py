78b0c7455474840898292a10926bfaa5100f481e
TakWolf <takwolf@foxmail.com>
2021-05-12 15:29:51 +0800

    修复发布脚本

--- services/publish_service.py
+++ services/publish_service.py
@@ -7,7 +7,7 @@ logger = logging.getLogger('publish-service')
 def make_release_zips(font_config):
     all_zip_file_path = font_config.zip_file_release_path
     with zipfile.ZipFile(all_zip_file_path, 'w') as all_zip_file:
-        for language_flavor_config in font_config.language_flavor_configs.values():
+        for language_flavor_config in font_config.language_flavor_configs:
             all_zip_file.write(language_flavor_config.otf_file_output_path, language_flavor_config.otf_file_name)
             all_zip_file.write(language_flavor_config.ttf_file_output_path, language_flavor_config.ttf_file_name)
             zip_file_path = language_flavor_config.zip_file_release_path
