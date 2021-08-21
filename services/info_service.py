0b9d8937f41cebc6f6518b544f76716b9e26fce8
TakWolf <takwolf@foxmail.com>
2021-08-22 04:21:44 +0800

    重构字体统计逻辑

--- services/info_service.py
+++ services/info_service.py
@@ -3,7 +3,7 @@ import logging
 import minify_html
 
 import configs
-from utils import unicode_util, gb2312_util, big5_util, shift_jis_util
+from utils import unicode_util, gb2312_util, big5_util, shift_jis_util, ks_x_1001_util
 
 logger = logging.getLogger('info-service')
 
@@ -22,60 +22,76 @@ def _get_unicode_char_count_infos(alphabet):
 
 
 def _get_gb2312_char_count_infos(alphabet):
-    level_1_count = 0
-    level_2_count = 0
-    other_count = 0
+    count_map = {}
     total_count = 0
     for c in alphabet:
         block_name = gb2312_util.query_block(c)
-        if block_name == 'level-1':
-            level_1_count += 1
-            total_count += 1
-        elif block_name == 'level-2':
-            level_2_count += 1
-            total_count += 1
-        elif block_name == 'other':
-            other_count += 1
+        if block_name:
+            block_count = count_map.get(block_name, 0)
+            block_count += 1
+            count_map[block_name] = block_count
             total_count += 1
     return [
-        ('一级汉字', level_1_count, gb2312_util.alphabet_level_1_count),
-        ('二级汉字', level_2_count, gb2312_util.alphabet_level_2_count),
-        ('其他字符', other_count, gb2312_util.alphabet_other_count),
+        ('一级汉字', count_map.get('level-1', 0), gb2312_util.alphabet_level_1_count),
+        ('二级汉字', count_map.get('level-2', 0), gb2312_util.alphabet_level_2_count),
+        ('其他字符', count_map.get('other', 0), gb2312_util.alphabet_other_count),
         ('总计', total_count, gb2312_util.alphabet_count)
     ]
 
 
 def _get_big5_char_count_infos(alphabet):
-    level_1_count = 0
-    level_2_count = 0
-    other_count = 0
+    count_map = {}
     total_count = 0
     for c in alphabet:
         block_name = big5_util.query_block(c)
-        if block_name == 'level-1':
-            level_1_count += 1
-            total_count += 1
-        elif block_name == 'level-2':
-            level_2_count += 1
-            total_count += 1
-        elif block_name == 'other':
-            other_count += 1
+        if block_name:
+            block_count = count_map.get(block_name, 0)
+            block_count += 1
+            count_map[block_name] = block_count
             total_count += 1
     return [
-        ('常用汉字', level_1_count, big5_util.alphabet_level_1_count),
-        ('次常用汉字', level_2_count, big5_util.alphabet_level_2_count),
-        ('标点符号、希腊字母、特殊符号，九个计量用汉字', other_count, big5_util.alphabet_other_count),
+        ('常用汉字', count_map.get('level-1', 0), big5_util.alphabet_level_1_count),
+        ('次常用汉字', count_map.get('level-2', 0), big5_util.alphabet_level_2_count),
+        ('标点符号、希腊字母、特殊符号，九个计量用汉字', count_map.get('other', 0), big5_util.alphabet_other_count),
         ('总计', total_count, big5_util.alphabet_count)
     ]
 
 
 def _get_shift_jis_char_count_infos(alphabet):
-    count = 0
+    count_map = {}
+    total_count = 0
     for c in alphabet:
-        if shift_jis_util.is_chr_include(c):
-            count += 1
-    total = len(shift_jis_util.get_alphabet())
-    return count, total
+        block_name = shift_jis_util.query_block(c)
+        if block_name:
+            block_count = count_map.get(block_name, 0)
+            block_count += 1
+            count_map[block_name] = block_count
+            total_count += 1
+    return [
+        ('单字节-ASCII字符', count_map.get('single-ascii', 0), shift_jis_util.alphabet_single_ascii_count),
+        ('单字节-半角标点和片假名', count_map.get('single-other', 0), shift_jis_util.alphabet_single_other_count),
+        ('双字节-假名和其他字符', count_map.get('double-basic', 0), shift_jis_util.alphabet_double_basic_count),
+        ('双字节-汉字', count_map.get('double-word', 0), shift_jis_util.alphabet_double_word_count),
+        ('总计', total_count, shift_jis_util.alphabet_count)
+    ]
+
+
+def _get_ks_x_1001_char_count_infos(alphabet):
+    count_map = {}
+    total_count = 0
+    for c in alphabet:
+        block_name = ks_x_1001_util.query_block(c)
+        if block_name:
+            block_count = count_map.get(block_name, 0)
+            block_count += 1
+            count_map[block_name] = block_count
+            total_count += 1
+    return [
+        ('谚文音节', count_map.get('syllable', 0), ks_x_1001_util.alphabet_syllable_count),
+        ('汉字', count_map.get('word', 0), ks_x_1001_util.alphabet_word_count),
+        ('其他字符', count_map.get('other', 0), ks_x_1001_util.alphabet_other_count),
+        ('总计', total_count, ks_x_1001_util.alphabet_count)
+    ]
 
 
 def _write_unicode_char_count_infos_table(file, infos):
@@ -87,28 +103,12 @@ def _write_unicode_char_count_infos_table(file, infos):
         file.write(f'| {code_point_range} | {unicode_block.name} | {unicode_block.name_cn if unicode_block.name_cn else ""} | {count} / {unicode_block.char_count} {finished_emoji} |\n')
 
 
-def _write_gb2312_char_count_infos_table(file, infos):
+def _write_locale_char_count_infos_table(file, infos):
     file.write('| 区块名称 | 覆盖情况 |\n')
     file.write('|---|---:|\n')
     for title, count, total in infos:
         finished_emoji = "🏆" if count == total else "🚧"
         file.write(f'| {title} | {count} / {total} {finished_emoji} |\n')
-
-
-def _write_big5_char_count_infos_table(file, infos):
-    file.write('| 区块名称 | 覆盖情况 |\n')
-    file.write('|---|---:|\n')
-    for title, count, total in infos:
-        finished_emoji = "🏆" if count == total else "🚧"
-        file.write(f'| {title} | {count} / {total} {finished_emoji} |\n')
-
-
-def _write_shift_jis_char_count_infos_table(file, infos):
-    file.write('| 覆盖情况 |\n')
-    file.write('|---:|\n')
-    count, total = infos
-    finished_emoji = "🏆" if count == total else "🚧"
-    file.write(f'| {count} / {total} {finished_emoji} |\n')
 
 
 def make_info_file(font_config, alphabet):
@@ -135,21 +135,27 @@ def make_info_file(font_config, alphabet):
         file.write('\n')
         file.write('## GB2312 字符分布\n')
         file.write('\n')
-        file.write('简体中文参考字符集。统计不包含 ASCII，和 Unicode 有交集。\n')
+        file.write('简体中文参考字符集。统计范围不包含 ASCII，和 Unicode 有交集。\n')
         file.write('\n')
-        _write_gb2312_char_count_infos_table(file, _get_gb2312_char_count_infos(alphabet))
+        _write_locale_char_count_infos_table(file, _get_gb2312_char_count_infos(alphabet))
         file.write('\n')
         file.write('## Big5 字符分布\n')
         file.write('\n')
-        file.write('繁体中文参考字符集。统计不包含 ASCII，和 Unicode 有交集。\n')
+        file.write('繁体中文参考字符集。统计范围不包含 ASCII，和 Unicode 有交集。\n')
         file.write('\n')
-        _write_big5_char_count_infos_table(file, _get_big5_char_count_infos(alphabet))
+        _write_locale_char_count_infos_table(file, _get_big5_char_count_infos(alphabet))
         file.write('\n')
         file.write('## Shift-JIS 字符分布\n')
         file.write('\n')
-        file.write('日语参考字符集。统计仅包含可打印字符，包含 ASCII，和 Unicode 有交集。\n')
+        file.write('日语参考字符集。和 Unicode 有交集。\n')
         file.write('\n')
-        _write_shift_jis_char_count_infos_table(file, _get_shift_jis_char_count_infos(alphabet))
+        _write_locale_char_count_infos_table(file, _get_shift_jis_char_count_infos(alphabet))
+        file.write('\n')
+        file.write('## KS X 1001 字符分布\n')
+        file.write('\n')
+        file.write('韩语参考字符集。统计范围不包含 ASCII，和 Unicode 有交集。\n')
+        file.write('\n')
+        _write_locale_char_count_infos_table(file, _get_ks_x_1001_char_count_infos(alphabet))
     logger.info(f'make {file_path}')
 
 
