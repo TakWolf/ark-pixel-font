ad01c7932417a076a85babfd920fbc21b6c68b8e
TakWolf <takwolf@foxmail.com>
2021-09-28 20:06:43 +0800

    修复发布脚本

--- services/publish_service.py
+++ services/publish_service.py
@@ -3,37 +3,41 @@ import os.path
 import shutil
 import zipfile
 
+import configs
 from configs import workspace_define
 
 logger = logging.getLogger('publish-service')
 
 
 def _make_otf_release_zip(font_config):
-    file_output_path = os.path.join(workspace_define.releases_dir, font_config.otf_zip_file_name)
+    file_output_path = os.path.join(workspace_define.releases_dir, font_config.get_release_zip_file_name('otf'))
     with zipfile.ZipFile(file_output_path, 'w') as zip_file:
-        for locale_flavor_config in font_config.locale_flavor_configs:
-            otf_file_path = os.path.join(workspace_define.outputs_dir, locale_flavor_config.otf_file_name)
-            zip_file.write(otf_file_path, locale_flavor_config.otf_file_name)
+        for locale_flavor in configs.locale_flavors:
+            otf_file_name = font_config.get_output_font_file_name(locale_flavor, 'otf')
+            otf_file_path = os.path.join(workspace_define.outputs_dir, otf_file_name)
+            zip_file.write(otf_file_path, otf_file_name)
         zip_file.write('LICENSE-OFL', 'OFL.txt')
     logger.info(f'make {file_output_path}')
 
 
 def _make_woff2_release_zip(font_config):
-    file_output_path = os.path.join(workspace_define.releases_dir, font_config.woff2_zip_file_name)
+    file_output_path = os.path.join(workspace_define.releases_dir, font_config.get_release_zip_file_name('woff2'))
     with zipfile.ZipFile(file_output_path, 'w') as zip_file:
-        for locale_flavor_config in font_config.locale_flavor_configs:
-            woff2_file_path = os.path.join(workspace_define.outputs_dir, locale_flavor_config.woff2_file_name)
-            zip_file.write(woff2_file_path, locale_flavor_config.woff2_file_name)
+        for locale_flavor in configs.locale_flavors:
+            woff2_file_name = font_config.get_output_font_file_name(locale_flavor, 'woff2')
+            woff2_file_path = os.path.join(workspace_define.outputs_dir, woff2_file_name)
+            zip_file.write(woff2_file_path, woff2_file_name)
         zip_file.write('LICENSE-OFL', 'OFL.txt')
     logger.info(f'make {file_output_path}')
 
 
 def _make_ttf_release_zip(font_config):
-    file_output_path = os.path.join(workspace_define.releases_dir, font_config.ttf_zip_file_name)
+    file_output_path = os.path.join(workspace_define.releases_dir, font_config.get_release_zip_file_name('ttf'))
     with zipfile.ZipFile(file_output_path, 'w') as zip_file:
-        for locale_flavor_config in font_config.locale_flavor_configs:
-            ttf_file_path = os.path.join(workspace_define.outputs_dir, locale_flavor_config.ttf_file_name)
-            zip_file.write(ttf_file_path, locale_flavor_config.ttf_file_name)
+        for locale_flavor in configs.locale_flavors:
+            ttf_file_name = font_config.get_output_font_file_name(locale_flavor, 'ttf')
+            ttf_file_path = os.path.join(workspace_define.outputs_dir, ttf_file_name)
+            zip_file.write(ttf_file_path, ttf_file_name)
         zip_file.write('LICENSE-OFL', 'OFL.txt')
     logger.info(f'make {file_output_path}')
 
@@ -45,9 +49,10 @@ def make_release_zips(font_config):
 
 
 def copy_docs_files(font_config):
-    for locale_flavor_config in font_config.locale_flavor_configs:
-        woff2_file_path = os.path.join(workspace_define.outputs_dir, locale_flavor_config.woff2_file_name)
-        woff2_file_docs_path = os.path.join(workspace_define.docs_dir, locale_flavor_config.woff2_file_name)
+    for locale_flavor in configs.locale_flavors:
+        woff2_file_name = font_config.get_output_font_file_name(locale_flavor, 'woff2')
+        woff2_file_path = os.path.join(workspace_define.outputs_dir, woff2_file_name)
+        woff2_file_docs_path = os.path.join(workspace_define.docs_dir, woff2_file_name)
         shutil.copy(woff2_file_path, woff2_file_docs_path)
         logger.info(f'copy from {woff2_file_path} to {woff2_file_docs_path}')
 
