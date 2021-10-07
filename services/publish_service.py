33a44169121f3c661be712dc85b76d27edb8bb39
TakWolf <takwolf@foxmail.com>
2021-10-08 02:08:12 +0800

    优化发布脚本

--- services/publish_service.py
+++ services/publish_service.py
@@ -9,43 +9,16 @@ from configs import workspace_define
 logger = logging.getLogger('publish-service')
 
 
-def _make_otf_release_zip(font_config):
-    file_output_path = os.path.join(workspace_define.releases_dir, font_config.get_release_zip_file_name('otf'))
-    with zipfile.ZipFile(file_output_path, 'w') as zip_file:
-        for locale_flavor in configs.locale_flavors:
-            otf_file_name = font_config.get_output_font_file_name(locale_flavor, 'otf')
-            otf_file_path = os.path.join(workspace_define.outputs_dir, otf_file_name)
-            zip_file.write(otf_file_path, otf_file_name)
-        zip_file.write('LICENSE-OFL', 'OFL.txt')
-    logger.info(f'make {file_output_path}')
-
-
-def _make_woff2_release_zip(font_config):
-    file_output_path = os.path.join(workspace_define.releases_dir, font_config.get_release_zip_file_name('woff2'))
-    with zipfile.ZipFile(file_output_path, 'w') as zip_file:
-        for locale_flavor in configs.locale_flavors:
-            woff2_file_name = font_config.get_output_font_file_name(locale_flavor, 'woff2')
-            woff2_file_path = os.path.join(workspace_define.outputs_dir, woff2_file_name)
-            zip_file.write(woff2_file_path, woff2_file_name)
-        zip_file.write('LICENSE-OFL', 'OFL.txt')
-    logger.info(f'make {file_output_path}')
-
-
-def _make_ttf_release_zip(font_config):
-    file_output_path = os.path.join(workspace_define.releases_dir, font_config.get_release_zip_file_name('ttf'))
-    with zipfile.ZipFile(file_output_path, 'w') as zip_file:
-        for locale_flavor in configs.locale_flavors:
-            ttf_file_name = font_config.get_output_font_file_name(locale_flavor, 'ttf')
-            ttf_file_path = os.path.join(workspace_define.outputs_dir, ttf_file_name)
-            zip_file.write(ttf_file_path, ttf_file_name)
-        zip_file.write('LICENSE-OFL', 'OFL.txt')
-    logger.info(f'make {file_output_path}')
-
-
 def make_release_zips(font_config):
-    _make_otf_release_zip(font_config)
-    _make_woff2_release_zip(font_config)
-    _make_ttf_release_zip(font_config)
+    for font_format in ['otf', 'woff2', 'ttf']:
+        zip_file_output_path = os.path.join(workspace_define.releases_dir, font_config.get_release_zip_file_name(font_format))
+        with zipfile.ZipFile(zip_file_output_path, 'w') as zip_file:
+            for locale_flavor in configs.locale_flavors:
+                font_file_name = font_config.get_output_font_file_name(locale_flavor, font_format)
+                font_file_path = os.path.join(workspace_define.outputs_dir, font_file_name)
+                zip_file.write(font_file_path, font_file_name)
+            zip_file.write('LICENSE-OFL', 'OFL.txt')
+        logger.info(f'make {zip_file_output_path}')
 
 
 def copy_docs_files(font_config):
@@ -56,22 +29,14 @@ def copy_docs_files(font_config):
         shutil.copy(woff2_file_path, woff2_file_docs_path)
         logger.info(f'copy from {woff2_file_path} to {woff2_file_docs_path}')
 
-    info_file_path = os.path.join(workspace_define.outputs_dir, font_config.info_file_name)
-    info_file_docs_path = os.path.join(workspace_define.docs_dir, font_config.info_file_name)
-    shutil.copy(info_file_path, info_file_docs_path)
-    logger.info(f'copy from {info_file_path} to {info_file_docs_path}')
-
-    preview_image_file_path = os.path.join(workspace_define.outputs_dir, font_config.preview_image_file_name)
-    preview_image_file_docs_path = os.path.join(workspace_define.docs_dir, font_config.preview_image_file_name)
-    shutil.copy(preview_image_file_path, preview_image_file_docs_path)
-    logger.info(f'copy from {preview_image_file_path} to {preview_image_file_docs_path}')
-
-    alphabet_html_file_path = os.path.join(workspace_define.outputs_dir, font_config.alphabet_html_file_name)
-    alphabet_html_file_docs_path = os.path.join(workspace_define.docs_dir, font_config.alphabet_html_file_name)
-    shutil.copy(alphabet_html_file_path, alphabet_html_file_docs_path)
-    logger.info(f'copy from {alphabet_html_file_path} to {alphabet_html_file_docs_path}')
-
-    demo_html_file_path = os.path.join(workspace_define.outputs_dir, font_config.demo_html_file_name)
-    demo_html_file_docs_path = os.path.join(workspace_define.docs_dir, font_config.demo_html_file_name)
-    shutil.copy(demo_html_file_path, demo_html_file_docs_path)
-    logger.info(f'copy from {demo_html_file_path} to {demo_html_file_docs_path}')
+    ext_file_names = [
+        font_config.info_file_name,
+        font_config.preview_image_file_name,
+        font_config.alphabet_html_file_name,
+        font_config.demo_html_file_name
+    ]
+    for ext_file_name in ext_file_names:
+        ext_file_path = os.path.join(workspace_define.outputs_dir, ext_file_name)
+        ext_file_docs_path = os.path.join(workspace_define.docs_dir, ext_file_name)
+        shutil.copy(ext_file_path, ext_file_docs_path)
+        logger.info(f'copy from {ext_file_path} to {ext_file_docs_path}')
