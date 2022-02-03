7fc8c8f487091453bad242667d096c37c6f3a22b
TakWolf <takwolf@foxmail.com>
2022-02-03 22:09:08 +0800

    资源路径定义调整为使用项目根目录相对路径

--- configs/workspace_define.py
+++ configs/workspace_define.py
@@ -1,13 +1,15 @@
 import os
 
-assets_dir = 'assets/'
+project_root_dir = os.path.join(os.path.dirname(__file__), '../')
+
+assets_dir = os.path.join(project_root_dir, 'assets/')
 unidata_dir = os.path.join(assets_dir, 'unidata/')
 design_dir = os.path.join(assets_dir, 'design/')
 templates_dir = os.path.join(assets_dir, 'templates/')
 images_dir = os.path.join(assets_dir, 'images/')
 
-outputs_dir = 'outputs/'
+outputs_dir = os.path.join(project_root_dir, 'outputs/')
 
-releases_dir = 'releases/'
+releases_dir = os.path.join(project_root_dir, 'releases/')
 
-docs_dir = 'docs/'
+docs_dir = os.path.join(project_root_dir, 'docs/')
