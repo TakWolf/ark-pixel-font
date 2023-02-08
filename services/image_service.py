ba6def06b37f97cd147405f40a30f96b72c21160
TakWolf <takwolf@foxmail.com>
2023-02-08 16:55:56 +0800

    图片物料生成 优化字体加载逻辑

--- services/image_service.py
+++ services/image_service.py
@@ -12,9 +12,9 @@ from utils import fs_util
 logger = logging.getLogger('image-service')
 
 
-def _load_font(px, width_mode, language_specific, size):
+def _load_font(px, width_mode, language_specific, px_scale=1):
     font_file_path = os.path.join(path_define.outputs_dir, configs.font_config_map[px].get_font_file_name(width_mode, language_specific, 'woff2'))
-    return ImageFont.truetype(font_file_path, size)
+    return ImageFont.truetype(font_file_path, px * px_scale)
 
 
 def _draw_text(image, xy, text, font, text_color=(0, 0, 0), shadow_color=None, line_height=None, line_gap=0, is_horizontal_centered=False, is_vertical_centered=False):
@@ -53,10 +53,10 @@ def _draw_text_background(image, alphabet, step, box_size, font, text_color):
 
 
 def make_preview_image_file(font_config):
-    font_latin = _load_font(font_config.px, 'proportional', 'latin', font_config.px)
-    font_zh_cn = _load_font(font_config.px, 'proportional', 'zh_cn', font_config.px)
-    font_zh_tr = _load_font(font_config.px, 'proportional', 'zh_tr', font_config.px)
-    font_ja = _load_font(font_config.px, 'proportional', 'ja', font_config.px)
+    font_latin = _load_font(font_config.px, 'proportional', 'latin')
+    font_zh_cn = _load_font(font_config.px, 'proportional', 'zh_cn')
+    font_zh_tr = _load_font(font_config.px, 'proportional', 'zh_tr')
+    font_ja = _load_font(font_config.px, 'proportional', 'ja')
 
     image = Image.new('RGBA', (font_config.px * 35, font_config.px * 2 + font_config.line_height_px * 8), (255, 255, 255))
     _draw_text(image, (font_config.px, font_config.px), '方舟像素字体 / Ark Pixel Font', font_zh_cn)
@@ -77,9 +77,8 @@ def make_preview_image_file(font_config):
 
 def make_readme_banner():
     alphabet = info_service.read_alphabet_txt_file(configs.font_config_map[12], 'proportional')
-    font_24_zh_cn = _load_font(12, 'proportional', 'zh_cn', 24)
-    font_12_zh_cn = _load_font(12, 'proportional', 'zh_cn', 12)
-
+    font_x1 = _load_font(12, 'proportional', 'zh_cn')
+    font_x2 = _load_font(12, 'proportional', 'zh_cn', 2)
     box_size = 14
     line_height = configs.font_config_map[12].line_height_px
     text_color = (255, 255, 255)
@@ -87,10 +86,10 @@ def make_readme_banner():
 
     image_background = Image.open(os.path.join(path_define.images_dir, 'readme-banner-background.png'))
     image = Image.new('RGBA', (image_background.width, image_background.height), (255, 255, 255, 0))
-    _draw_text_background(image, alphabet, 12, box_size, font_12_zh_cn, (200, 200, 200))
+    _draw_text_background(image, alphabet, 12, box_size, font_x1, (200, 200, 200))
     image.paste(image_background, mask=image_background)
-    _draw_text(image, (image.width / 2, 28), '方舟像素字体', font_24_zh_cn, text_color=text_color, shadow_color=shadow_color, is_horizontal_centered=True)
-    _draw_text(image, (image.width / 2, 28 + line_height * 2 + 4), '★ 开源的泛中日韩像素字体 ★', font_12_zh_cn, text_color=text_color, shadow_color=shadow_color, is_horizontal_centered=True)
+    _draw_text(image, (image.width / 2, 28), '方舟像素字体', font_x2, text_color=text_color, shadow_color=shadow_color, is_horizontal_centered=True)
+    _draw_text(image, (image.width / 2, 28 + line_height * 2 + 4), '★ 开源的泛中日韩像素字体 ★', font_x1, text_color=text_color, shadow_color=shadow_color, is_horizontal_centered=True)
     image = image.resize((image.width * 2, image.height * 2), Image.NEAREST)
 
     fs_util.make_dirs_if_not_exists(path_define.outputs_dir)
@@ -101,12 +100,11 @@ def make_readme_banner():
 
 def make_github_banner():
     alphabet = info_service.read_alphabet_txt_file(configs.font_config_map[12], 'proportional')
-    font_24_zh_cn = _load_font(12, 'proportional', 'zh_cn', 24)
-    font_12_latin = _load_font(12, 'proportional', 'latin', 12)
-    font_12_zh_cn = _load_font(12, 'proportional', 'zh_cn', 12)
-    font_12_zh_tr = _load_font(12, 'proportional', 'zh_tr', 12)
-    font_12_ja = _load_font(12, 'proportional', 'ja', 12)
-
+    font_title = _load_font(12, 'proportional', 'zh_cn', 2)
+    font_latin = _load_font(12, 'proportional', 'latin')
+    font_zh_cn = _load_font(12, 'proportional', 'zh_cn')
+    font_zh_tr = _load_font(12, 'proportional', 'zh_tr')
+    font_ja = _load_font(12, 'proportional', 'ja')
     box_size = 14
     line_height = configs.font_config_map[12].line_height_px
     text_color = (255, 255, 255)
@@ -114,17 +112,17 @@ def make_github_banner():
 
     image_background = Image.open(os.path.join(path_define.images_dir, 'github-banner-background.png'))
     image = Image.new('RGBA', (image_background.width, image_background.height), (255, 255, 255, 0))
-    _draw_text_background(image, alphabet, 6, box_size, font_12_zh_cn, (200, 200, 200))
+    _draw_text_background(image, alphabet, 6, box_size, font_zh_cn, (200, 200, 200))
     image.paste(image_background, mask=image_background)
-    _draw_text(image, (image.width / 2, 40 + line_height), '方舟像素字体 / Ark Pixel Font', font_24_zh_cn, text_color=text_color, shadow_color=shadow_color, is_horizontal_centered=True)
-    _draw_text(image, (image.width / 2, 40 + line_height * 3), '★ 开源的泛中日韩像素字体 ★', font_12_zh_cn, text_color=text_color, shadow_color=shadow_color, is_horizontal_centered=True)
-    _draw_text(image, (image.width / 2, 40 + line_height * 5), '我们每天度过的称之为日常的生活，其实是一个个奇迹的连续也说不定。', font_12_zh_cn, text_color=text_color, shadow_color=shadow_color, is_horizontal_centered=True)
-    _draw_text(image, (image.width / 2, 40 + line_height * 6), '我們每天度過的稱之為日常的生活，其實是一個個奇跡的連續也說不定。', font_12_zh_tr, text_color=text_color, shadow_color=shadow_color, is_horizontal_centered=True)
-    _draw_text(image, (image.width / 2, 40 + line_height * 7), '日々、私たちが過ごしている日常は、実は奇跡の連続なのかもしれない。', font_12_ja, text_color=text_color, shadow_color=shadow_color, is_horizontal_centered=True)
-    _draw_text(image, (image.width / 2, 40 + line_height * 8), 'THE QUICK BROWN FOX JUMPS OVER A LAZY DOG.', font_12_latin, text_color=text_color, shadow_color=shadow_color, is_horizontal_centered=True)
-    _draw_text(image, (image.width / 2, 40 + line_height * 9), 'the quick brown fox jumps over a lazy dog.', font_12_latin, text_color=text_color, shadow_color=shadow_color, is_horizontal_centered=True)
-    _draw_text(image, (image.width / 2, 40 + line_height * 10), '0123456789', font_12_latin, text_color=text_color, shadow_color=shadow_color, is_horizontal_centered=True)
-    _draw_text(image, (image.width / 2, 40 + line_height * 11), '★☆☺☹♠♡♢♣♤♥♦♧☀☼♩♪♫♬☂☁⚓✈⚔☯', font_12_latin, text_color=text_color, shadow_color=shadow_color, is_horizontal_centered=True)
+    _draw_text(image, (image.width / 2, 40 + line_height), '方舟像素字体 / Ark Pixel Font', font_title, text_color=text_color, shadow_color=shadow_color, is_horizontal_centered=True)
+    _draw_text(image, (image.width / 2, 40 + line_height * 3), '★ 开源的泛中日韩像素字体 ★', font_zh_cn, text_color=text_color, shadow_color=shadow_color, is_horizontal_centered=True)
+    _draw_text(image, (image.width / 2, 40 + line_height * 5), '我们每天度过的称之为日常的生活，其实是一个个奇迹的连续也说不定。', font_zh_cn, text_color=text_color, shadow_color=shadow_color, is_horizontal_centered=True)
+    _draw_text(image, (image.width / 2, 40 + line_height * 6), '我們每天度過的稱之為日常的生活，其實是一個個奇跡的連續也說不定。', font_zh_tr, text_color=text_color, shadow_color=shadow_color, is_horizontal_centered=True)
+    _draw_text(image, (image.width / 2, 40 + line_height * 7), '日々、私たちが過ごしている日常は、実は奇跡の連続なのかもしれない。', font_ja, text_color=text_color, shadow_color=shadow_color, is_horizontal_centered=True)
+    _draw_text(image, (image.width / 2, 40 + line_height * 8), 'THE QUICK BROWN FOX JUMPS OVER A LAZY DOG.', font_latin, text_color=text_color, shadow_color=shadow_color, is_horizontal_centered=True)
+    _draw_text(image, (image.width / 2, 40 + line_height * 9), 'the quick brown fox jumps over a lazy dog.', font_latin, text_color=text_color, shadow_color=shadow_color, is_horizontal_centered=True)
+    _draw_text(image, (image.width / 2, 40 + line_height * 10), '0123456789', font_latin, text_color=text_color, shadow_color=shadow_color, is_horizontal_centered=True)
+    _draw_text(image, (image.width / 2, 40 + line_height * 11), '★☆☺☹♠♡♢♣♤♥♦♧☀☼♩♪♫♬☂☁⚓✈⚔☯', font_latin, text_color=text_color, shadow_color=shadow_color, is_horizontal_centered=True)
     image = image.resize((image.width * 2, image.height * 2), Image.NEAREST)
 
     fs_util.make_dirs_if_not_exists(path_define.outputs_dir)
@@ -135,9 +133,8 @@ def make_github_banner():
 
 def make_itch_io_banner():
     alphabet = info_service.read_alphabet_txt_file(configs.font_config_map[12], 'proportional')
-    font_24_zh_cn = _load_font(12, 'proportional', 'zh_cn', 24)
-    font_12_zh_cn = _load_font(12, 'proportional', 'zh_cn', 12)
-
+    font_x1 = _load_font(12, 'proportional', 'zh_cn')
+    font_x2 = _load_font(12, 'proportional', 'zh_cn', 2)
     box_size = 14
     line_height = configs.font_config_map[12].line_height_px
     text_color = (255, 255, 255)
@@ -145,10 +142,10 @@ def make_itch_io_banner():
 
     image_background = Image.open(os.path.join(path_define.images_dir, 'itch-io-banner-background.png'))
     image = Image.new('RGBA', (image_background.width, image_background.height), (255, 255, 255, 0))
-    _draw_text_background(image, alphabet, 12, box_size, font_12_zh_cn, (200, 200, 200))
+    _draw_text_background(image, alphabet, 12, box_size, font_x1, (200, 200, 200))
     image.paste(image_background, mask=image_background)
-    _draw_text(image, (image.width / 2, 32), '方舟像素字体 / Ark Pixel Font', font_24_zh_cn, text_color=text_color, shadow_color=shadow_color, is_horizontal_centered=True)
-    _draw_text(image, (image.width / 2, 32 + line_height * 2 + 4), '★ 开源的泛中日韩像素字体 ★', font_12_zh_cn, text_color=text_color, shadow_color=shadow_color, is_horizontal_centered=True)
+    _draw_text(image, (image.width / 2, 32), '方舟像素字体 / Ark Pixel Font', font_x2, text_color=text_color, shadow_color=shadow_color, is_horizontal_centered=True)
+    _draw_text(image, (image.width / 2, 32 + line_height * 2 + 4), '★ 开源的泛中日韩像素字体 ★', font_x1, text_color=text_color, shadow_color=shadow_color, is_horizontal_centered=True)
     image = image.resize((image.width * 2, image.height * 2), Image.NEAREST)
 
     fs_util.make_dirs_if_not_exists(path_define.outputs_dir)
@@ -159,12 +156,11 @@ def make_itch_io_banner():
 
 def make_itch_io_background():
     alphabet = info_service.read_alphabet_txt_file(configs.font_config_map[12], 'proportional')
-    font_12_zh_cn = _load_font(12, 'proportional', 'zh_cn', 12)
-
+    font = _load_font(12, 'proportional', 'zh_cn')
     box_size = 14
 
     image = Image.new('RGBA', (box_size * 50, box_size * 50), (255, 255, 255, 0))
-    _draw_text_background(image, alphabet, 2, box_size, font_12_zh_cn, (30, 30, 30))
+    _draw_text_background(image, alphabet, 2, box_size, font, (30, 30, 30))
     image = image.resize((image.width * 2, image.height * 2), Image.NEAREST)
 
     fs_util.make_dirs_if_not_exists(path_define.outputs_dir)
@@ -174,25 +170,24 @@ def make_itch_io_background():
 
 
 def make_itch_io_cover():
-    font_24_zh_cn = _load_font(12, 'proportional', 'zh_cn', 24)
-    font_12_latin = _load_font(12, 'proportional', 'latin', 12)
-    font_12_zh_cn = _load_font(12, 'proportional', 'zh_cn', 12)
-    font_12_zh_tr = _load_font(12, 'proportional', 'zh_tr', 12)
-    font_12_ja = _load_font(12, 'proportional', 'ja', 12)
-
+    font_title = _load_font(12, 'proportional', 'zh_cn', 2)
+    font_latin = _load_font(12, 'proportional', 'latin')
+    font_zh_cn = _load_font(12, 'proportional', 'zh_cn')
+    font_zh_tr = _load_font(12, 'proportional', 'zh_tr')
+    font_ja = _load_font(12, 'proportional', 'ja')
     line_height = configs.font_config_map[12].line_height_px
     text_color = (255, 255, 255)
     shadow_color = (80, 80, 80)
 
     image = Image.open(os.path.join(path_define.images_dir, 'itch-io-cover-background.png'))
-    _draw_text(image, (image.width / 2, 6), '方舟像素字体', font_24_zh_cn, text_color=text_color, shadow_color=shadow_color, is_horizontal_centered=True)
-    _draw_text(image, (image.width / 2, 6 + line_height * 2), '我们每天度过的称之为日常的生活，\n其实是一个个奇迹的连续也说不定。', font_12_zh_cn, text_color=text_color, shadow_color=shadow_color, is_horizontal_centered=True)
-    _draw_text(image, (image.width / 2, 6 + line_height * 4), '我們每天度過的稱之為日常的生活，\n其實是一個個奇跡的連續也說不定。', font_12_zh_tr, text_color=text_color, shadow_color=shadow_color, is_horizontal_centered=True)
-    _draw_text(image, (image.width / 2, 6 + line_height * 6), '日々、私たちが過ごしている日常は、\n 実は奇跡の連続なのかもしれない。', font_12_ja, text_color=text_color, shadow_color=shadow_color, is_horizontal_centered=True)
-    _draw_text(image, (image.width / 2, 6 + line_height * 8), 'THE QUICK BROWN FOX JUMPS OVER A LAZY DOG.', font_12_latin, text_color=text_color, shadow_color=shadow_color, is_horizontal_centered=True)
-    _draw_text(image, (image.width / 2, 6 + line_height * 9), 'the quick brown fox jumps over a lazy dog.', font_12_latin, text_color=text_color, shadow_color=shadow_color, is_horizontal_centered=True)
-    _draw_text(image, (image.width / 2, 6 + line_height * 10), '0123456789', font_12_latin, text_color=text_color, shadow_color=shadow_color, is_horizontal_centered=True)
-    _draw_text(image, (image.width / 2, 6 + line_height * 11), '★☆☺☹♠♡♢♣♤♥♦♧\n☀☼♩♪♫♬☂☁⚓✈⚔☯', font_12_latin, text_color=text_color, shadow_color=shadow_color, is_horizontal_centered=True)
+    _draw_text(image, (image.width / 2, 6), '方舟像素字体', font_title, text_color=text_color, shadow_color=shadow_color, is_horizontal_centered=True)
+    _draw_text(image, (image.width / 2, 6 + line_height * 2), '我们每天度过的称之为日常的生活，\n其实是一个个奇迹的连续也说不定。', font_zh_cn, text_color=text_color, shadow_color=shadow_color, is_horizontal_centered=True)
+    _draw_text(image, (image.width / 2, 6 + line_height * 4), '我們每天度過的稱之為日常的生活，\n其實是一個個奇跡的連續也說不定。', font_zh_tr, text_color=text_color, shadow_color=shadow_color, is_horizontal_centered=True)
+    _draw_text(image, (image.width / 2, 6 + line_height * 6), '日々、私たちが過ごしている日常は、\n 実は奇跡の連続なのかもしれない。', font_ja, text_color=text_color, shadow_color=shadow_color, is_horizontal_centered=True)
+    _draw_text(image, (image.width / 2, 6 + line_height * 8), 'THE QUICK BROWN FOX JUMPS OVER A LAZY DOG.', font_latin, text_color=text_color, shadow_color=shadow_color, is_horizontal_centered=True)
+    _draw_text(image, (image.width / 2, 6 + line_height * 9), 'the quick brown fox jumps over a lazy dog.', font_latin, text_color=text_color, shadow_color=shadow_color, is_horizontal_centered=True)
+    _draw_text(image, (image.width / 2, 6 + line_height * 10), '0123456789', font_latin, text_color=text_color, shadow_color=shadow_color, is_horizontal_centered=True)
+    _draw_text(image, (image.width / 2, 6 + line_height * 11), '★☆☺☹♠♡♢♣♤♥♦♧\n☀☼♩♪♫♬☂☁⚓✈⚔☯', font_latin, text_color=text_color, shadow_color=shadow_color, is_horizontal_centered=True)
     image = image.resize((image.width * 2, image.height * 2), Image.NEAREST)
 
     fs_util.make_dirs_if_not_exists(path_define.outputs_dir)
@@ -202,27 +197,26 @@ def make_itch_io_cover():
 
 
 def make_afdian_cover():
-    font_24_zh_cn = _load_font(12, 'proportional', 'zh_cn', 24)
-    font_12_latin = _load_font(12, 'proportional', 'latin', 12)
-    font_12_zh_cn = _load_font(12, 'proportional', 'zh_cn', 12)
-    font_12_zh_tr = _load_font(12, 'proportional', 'zh_tr', 12)
-    font_12_ja = _load_font(12, 'proportional', 'ja', 12)
-
+    font_title = _load_font(12, 'proportional', 'zh_cn', 2)
+    font_latin = _load_font(12, 'proportional', 'latin')
+    font_zh_cn = _load_font(12, 'proportional', 'zh_cn')
+    font_zh_tr = _load_font(12, 'proportional', 'zh_tr')
+    font_ja = _load_font(12, 'proportional', 'ja')
     line_height = configs.font_config_map[12].line_height_px
     text_color = (255, 255, 255)
     shadow_color = (80, 80, 80)
 
     image = Image.open(os.path.join(path_define.images_dir, 'afdian-cover-background.png'))
-    _draw_text(image, (image.width / 2, 12), '方舟像素字体', font_24_zh_cn, text_color=text_color, shadow_color=shadow_color, is_horizontal_centered=True)
-    _draw_text(image, (image.width / 2, 12 + line_height * 2), 'Ark Pixel Font', font_12_zh_cn, text_color=text_color, shadow_color=shadow_color, is_horizontal_centered=True)
-    _draw_text(image, (image.width / 2, 18 + line_height * 3), '★ 开源的泛中日韩像素字体 ★', font_12_zh_cn, text_color=text_color, shadow_color=shadow_color, is_horizontal_centered=True)
-    _draw_text(image, (image.width / 2, 18 + line_height * 5), '我们每天度过的称之为日常的生活，\n其实是一个个奇迹的连续也说不定。', font_12_zh_cn, text_color=text_color, shadow_color=shadow_color, is_horizontal_centered=True)
-    _draw_text(image, (image.width / 2, 18 + line_height * 7), '我們每天度過的稱之為日常的生活，\n其實是一個個奇跡的連續也說不定。', font_12_zh_tr, text_color=text_color, shadow_color=shadow_color, is_horizontal_centered=True)
-    _draw_text(image, (image.width / 2, 18 + line_height * 9), '日々、私たちが過ごしている日常は、\n 実は奇跡の連続なのかもしれない。', font_12_ja, text_color=text_color, shadow_color=shadow_color, is_horizontal_centered=True)
-    _draw_text(image, (image.width / 2, 18 + line_height * 11), 'THE QUICK BROWN FOX JUMPS OVER A LAZY DOG.', font_12_latin, text_color=text_color, shadow_color=shadow_color, is_horizontal_centered=True)
-    _draw_text(image, (image.width / 2, 18 + line_height * 12), 'the quick brown fox jumps over a lazy dog.', font_12_latin, text_color=text_color, shadow_color=shadow_color, is_horizontal_centered=True)
-    _draw_text(image, (image.width / 2, 18 + line_height * 13), '0123456789', font_12_latin, text_color=text_color, shadow_color=shadow_color, is_horizontal_centered=True)
-    _draw_text(image, (image.width / 2, 18 + line_height * 14), '★☆☺☹♠♡♢♣♤♥♦♧\n☀☼♩♪♫♬☂☁⚓✈⚔☯', font_12_latin, text_color=text_color, shadow_color=shadow_color, is_horizontal_centered=True)
+    _draw_text(image, (image.width / 2, 12), '方舟像素字体', font_title, text_color=text_color, shadow_color=shadow_color, is_horizontal_centered=True)
+    _draw_text(image, (image.width / 2, 12 + line_height * 2), 'Ark Pixel Font', font_zh_cn, text_color=text_color, shadow_color=shadow_color, is_horizontal_centered=True)
+    _draw_text(image, (image.width / 2, 18 + line_height * 3), '★ 开源的泛中日韩像素字体 ★', font_zh_cn, text_color=text_color, shadow_color=shadow_color, is_horizontal_centered=True)
+    _draw_text(image, (image.width / 2, 18 + line_height * 5), '我们每天度过的称之为日常的生活，\n其实是一个个奇迹的连续也说不定。', font_zh_cn, text_color=text_color, shadow_color=shadow_color, is_horizontal_centered=True)
+    _draw_text(image, (image.width / 2, 18 + line_height * 7), '我們每天度過的稱之為日常的生活，\n其實是一個個奇跡的連續也說不定。', font_zh_tr, text_color=text_color, shadow_color=shadow_color, is_horizontal_centered=True)
+    _draw_text(image, (image.width / 2, 18 + line_height * 9), '日々、私たちが過ごしている日常は、\n 実は奇跡の連続なのかもしれない。', font_ja, text_color=text_color, shadow_color=shadow_color, is_horizontal_centered=True)
+    _draw_text(image, (image.width / 2, 18 + line_height * 11), 'THE QUICK BROWN FOX JUMPS OVER A LAZY DOG.', font_latin, text_color=text_color, shadow_color=shadow_color, is_horizontal_centered=True)
+    _draw_text(image, (image.width / 2, 18 + line_height * 12), 'the quick brown fox jumps over a lazy dog.', font_latin, text_color=text_color, shadow_color=shadow_color, is_horizontal_centered=True)
+    _draw_text(image, (image.width / 2, 18 + line_height * 13), '0123456789', font_latin, text_color=text_color, shadow_color=shadow_color, is_horizontal_centered=True)
+    _draw_text(image, (image.width / 2, 18 + line_height * 14), '★☆☺☹♠♡♢♣♤♥♦♧\n☀☼♩♪♫♬☂☁⚓✈⚔☯', font_latin, text_color=text_color, shadow_color=shadow_color, is_horizontal_centered=True)
     image = image.resize((image.width * 2, image.height * 2), Image.NEAREST)
 
     fs_util.make_dirs_if_not_exists(path_define.outputs_dir)
