a8ae56032f1a1969e596d3e6deb95db5f852649b
TakWolf <takwolf@foxmail.com>
2023-08-11 14:34:41 +0800

    代码整理

--- utils/fs_util.py
+++ utils/fs_util.py
@@ -1,6 +1,5 @@
 import os
 import shutil
-from collections.abc import Iterator
 
 
 def delete_dir(path: str):
@@ -16,12 +15,3 @@ def make_dirs(path: str):
             raise Exception(f"Path exists but not a directory: '{path}'")
     else:
         os.makedirs(path)
-
-
-def walk_files(path: str) -> Iterator[tuple[str, str]]:
-    if os.path.exists(path):
-        if not os.path.isdir(path):
-            raise Exception(f"Path not a directory: '{path}'")
-        for parent, _, names in os.walk(path):
-            for name in names:
-                yield parent, name
