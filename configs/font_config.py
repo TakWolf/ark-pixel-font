174c91c022ec2b976bef4519b5b767bda69b747a
TakWolf <takwolf@foxmail.com>
2023-11-26 06:55:38 +0800

    版本号时间使用 UTC+0 时区

--- configs/font_config.py
+++ configs/font_config.py
@@ -1,5 +1,5 @@
+import datetime
 import os
-import time
 import tomllib
 from typing import Final
 
@@ -20,7 +20,7 @@ class Metrics:
 
 
 class FontConfig:
-    VERSION: Final[str] = time.strftime("%Y.%m.%d")
+    VERSION: Final[str] = datetime.datetime.now(datetime.UTC).strftime("%Y.%m.%d")
     FAMILY_NAME: Final[str] = 'Ark Pixel'
     OUTPUTS_NAME: Final[str] = 'ark-pixel'
     MANUFACTURER: Final[str] = 'TakWolf'
