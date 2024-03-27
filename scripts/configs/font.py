42334e846ddb148d5e36f62b36685b0d2e8aaa38
TakWolf <takwolf@foxmail.com>
2024-03-27 10:56:25 +0800

    2024.03.27

--- scripts/configs/font.py
+++ scripts/configs/font.py
@@ -24,7 +24,7 @@ class LayoutParam:
 
 
 class FontConfig:
-    VERSION: Final[str] = '2024.03.22'
+    VERSION: Final[str] = '2024.03.27'
     VERSION_TIME: Final[datetime.datetime] = datetime.datetime.fromisoformat(f'{VERSION.replace('.', '-')}T00:00:00Z')
     FAMILY_NAME: Final[str] = 'Ark Pixel'
     OUTPUTS_NAME: Final[str] = 'ark-pixel'
