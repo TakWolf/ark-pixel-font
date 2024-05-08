c7a03560e5c4fdd2e40e2819802df1383fca1775
TakWolf <takwolf@foxmail.com>
2024-05-08 17:09:32 +0800

    移除 bitmap 优化逻辑

--- scripts/services/font_service.py
+++ scripts/services/font_service.py
@@ -10,8 +10,7 @@ from pixel_font_builder.opentype import Flavor
 
 from scripts import configs
 from scripts.configs import path_define, FontConfig
-from scripts.utils import fs_util
-from scripts.utils.bitmap_util import CroppedBitmap
+from scripts.utils import fs_util, bitmap_util
 
 logger = logging.getLogger('font_service')
 
@@ -39,7 +38,7 @@ class GlyphFile:
 
     def __init__(self, file_path: str, code_point: int, language_flavors: list[str]):
         self.file_path = file_path
-        self.bitmap = CroppedBitmap.load(file_path)
+        self.bitmap, self.width, self.height = bitmap_util.load_png(file_path)
         self.code_point = code_point
         self.language_flavors = language_flavors
 
@@ -127,27 +126,27 @@ class DesignContext:
                             file_dir = os.path.join(file_dir, f'{hex_name[0:-2]}-')
 
                     if width_mode_dir_name == 'common' or width_mode_dir_name == 'monospaced':
-                        assert glyph_file.bitmap.raw_height == self.font_config.size, f"Glyph data error: '{glyph_file.file_path}'"
+                        assert glyph_file.height == self.font_config.size, f"Glyph data error: '{glyph_file.file_path}'"
 
                         # H/Halfwidth or Na/Narrow
                         if east_asian_width == 'H' or east_asian_width == 'Na':
-                            assert glyph_file.bitmap.raw_width == self.font_config.size / 2, f"Glyph data error: '{glyph_file.file_path}'"
+                            assert glyph_file.width == self.font_config.size / 2, f"Glyph data error: '{glyph_file.file_path}'"
                         # F/Fullwidth or W/Wide
                         elif east_asian_width == 'F' or east_asian_width == 'W':
-                            assert glyph_file.bitmap.raw_width == self.font_config.size, f"Glyph data error: '{glyph_file.file_path}'"
+                            assert glyph_file.width == self.font_config.size, f"Glyph data error: '{glyph_file.file_path}'"
                         # A/Ambiguous or N/Neutral
                         else:
-                            assert glyph_file.bitmap.raw_width == self.font_config.size / 2 or glyph_file.bitmap.raw_width == self.font_config.size, f"Glyph data error: '{glyph_file.file_path}'"
+                            assert glyph_file.width == self.font_config.size / 2 or glyph_file.width == self.font_config.size, f"Glyph data error: '{glyph_file.file_path}'"
 
                         if block is not None:
                             if 'CJK Unified Ideographs' in block.name:
-                                assert all(glyph_file.bitmap.index_raw_data(raw_x, 0) == 0 for raw_x in range(glyph_file.bitmap.raw_width)), f"Glyph data error: '{glyph_file.file_path}'"
-                                assert all(glyph_file.bitmap.index_raw_data(-1, raw_y) == 0 for raw_y in range(glyph_file.bitmap.raw_height)), f"Glyph data error: '{glyph_file.file_path}'"
+                                assert all(alpha == 0 for alpha in glyph_file.bitmap[0]), f"Glyph data error: '{glyph_file.file_path}'"
+                                assert all(glyph_file.bitmap[i][-1] == 0 for i in range(0, len(glyph_file.bitmap))), f"Glyph data error: '{glyph_file.file_path}'"
 
                     if width_mode_dir_name == 'proportional':
-                        assert glyph_file.bitmap.raw_height == self.font_config.line_height, f"Glyph data error: '{glyph_file.file_path}'"
+                        assert glyph_file.height == self.font_config.line_height, f"Glyph data error: '{glyph_file.file_path}'"
 
-                    glyph_file.bitmap.save(glyph_file.file_path)
+                    bitmap_util.save_png(glyph_file.bitmap, glyph_file.file_path)
 
                     file_path = os.path.join(file_dir, file_name)
                     if glyph_file.file_path != file_path:
@@ -268,16 +267,15 @@ def _create_builder(
         if glyph_file.file_path in glyph_pool:
             glyph = glyph_pool[glyph_file.file_path]
         else:
-            horizontal_origin_x = glyph_file.bitmap.cropped_left
-            horizontal_origin_y = math.floor((layout_param.ascent + layout_param.descent - glyph_file.bitmap.raw_height) / 2) + glyph_file.bitmap.cropped_bottom
-            vertical_origin_y = (design_context.font_config.size - glyph_file.bitmap.raw_height) // 2 - 1 + glyph_file.bitmap.cropped_top
+            horizontal_origin_y = math.floor((layout_param.ascent + layout_param.descent - glyph_file.height) / 2)
+            vertical_origin_y = (design_context.font_config.size - glyph_file.height) // 2 - 1
             glyph = Glyph(
                 name=glyph_file.glyph_name,
-                advance_width=glyph_file.bitmap.raw_width,
+                advance_width=glyph_file.width,
                 advance_height=design_context.font_config.size,
-                horizontal_origin=(horizontal_origin_x, horizontal_origin_y),
+                horizontal_origin=(0, horizontal_origin_y),
                 vertical_origin_y=vertical_origin_y,
-                bitmap=glyph_file.bitmap.data,
+                bitmap=glyph_file.bitmap,
             )
             glyph_pool[glyph_file.file_path] = glyph
         builder.glyphs.append(glyph)
--- scripts/utils/bitmap_util.py
+++ scripts/utils/bitmap_util.py
@@ -5,126 +5,27 @@ import png
 
 def load_png(file_path: str | bytes | os.PathLike[str] | os.PathLike[bytes]) -> tuple[list[list[int]], int, int]:
     width, height, pixels, _ = png.Reader(filename=file_path).read()
-    data = []
+    bitmap = []
     for pixels_row in pixels:
-        data_row = []
-        for x in range(0, width * 4, 4):
-            alpha = pixels_row[x + 3]
-            if alpha > 127:
-                data_row.append(1)
-            else:
-                data_row.append(0)
-        data.append(data_row)
-    return data, width, height
+        bitmap_row = []
+        for i in range(0, width * 4, 4):
+            alpha = pixels_row[i + 3]
+            bitmap_row.append(1 if alpha > 127 else 0)
+        bitmap.append(bitmap_row)
+    return bitmap, width, height
 
 
 def save_png(
-        data: list[list[int]],
+        bitmap: list[list[int]],
         file_path: str | bytes | os.PathLike[str] | os.PathLike[bytes],
 ):
     pixels = []
-    for data_row in data:
+    for bitmap_row in bitmap:
         pixels_row = []
-        for x in data_row:
+        for alpha in bitmap_row:
             pixels_row.append(0)
             pixels_row.append(0)
             pixels_row.append(0)
-            if x == 0:
-                pixels_row.append(0)
-            else:
-                pixels_row.append(255)
+            pixels_row.append(255 if alpha != 0 else 0)
         pixels.append(pixels_row)
     png.from_array(pixels, 'RGBA').save(file_path)
-
-
-class CroppedBitmap:
-    @staticmethod
-    def load(file_path: str | bytes | os.PathLike[str] | os.PathLike[bytes]) -> 'CroppedBitmap':
-        data, width, height = load_png(file_path)
-
-        cropped_left = 0
-        cropped_right = 0
-        cropped_top = 0
-        cropped_bottom = 0
-        # Top
-        while height > 1:
-            if any(alpha != 0 for alpha in data[0]):
-                break
-            data.pop(0)
-            cropped_top += 1
-            height -= 1
-        # Bottom
-        while height > 1:
-            if any(alpha != 0 for alpha in data[-1]):
-                break
-            data.pop()
-            cropped_bottom += 1
-            height -= 1
-        # Right
-        while width > 1:
-            if any(data_row[-1] != 0 for data_row in data):
-                break
-            for data_row in data:
-                data_row.pop()
-            cropped_right += 1
-            width -= 1
-        # Left
-        while width > 1:
-            if any(data_row[0] != 0 for data_row in data):
-                break
-            for data_row in data:
-                data_row.pop(0)
-            cropped_left += 1
-            width -= 1
-
-        return CroppedBitmap(data, width, height, cropped_left, cropped_right, cropped_top, cropped_bottom)
-
-    def __init__(
-            self,
-            data: list[list[int]],
-            width: int,
-            height: int,
-            cropped_left: int,
-            cropped_right: int,
-            cropped_top: int,
-            cropped_bottom: int,
-    ):
-        self.data = data
-        self.width = width
-        self.height = height
-        self.cropped_left = cropped_left
-        self.cropped_right = cropped_right
-        self.cropped_top = cropped_top
-        self.cropped_bottom = cropped_bottom
-
-    @property
-    def raw_width(self) -> int:
-        return self.width + self.cropped_left + self.cropped_right
-
-    @property
-    def raw_height(self) -> int:
-        return self.height + self.cropped_top + self.cropped_bottom
-
-    def index_raw_data(self, raw_x: int, raw_y: int) -> int:
-        if raw_x < 0:
-            raw_x += self.raw_width
-        if raw_y < 0:
-            raw_y += self.raw_height
-        x = raw_x - self.cropped_left
-        y = raw_y - self.cropped_top
-        if 0 <= x < self.width and 0 <= y < self.height:
-            return self.data[y][x]
-        else:
-            return 0
-
-    def save(self, file_path: str | bytes | os.PathLike[str] | os.PathLike[bytes]):
-        data = []
-        for _ in range(self.cropped_top):
-            data.append([0] * (self.width + self.cropped_left + self.cropped_right))
-        for data_row in self.data:
-            data_row = [0] * self.cropped_left + data_row + [0] * self.cropped_right
-            data.append(data_row)
-        for _ in range(self.cropped_bottom):
-            data.append([0] * (self.width + self.cropped_left + self.cropped_right))
-
-        save_png(data, file_path)
