8ab292a5cca59dbe5de53e0354c23f6c584d0eae
TakWolf <takwolf@foxmail.com>
2024-06-05 10:58:59 +0800

    调整 version_time 配置

--- scripts/configs/__init__.py
+++ scripts/configs/__init__.py
@@ -1,11 +1,7 @@
-import datetime
-
 from scripts.configs.deploy import GitDeployConfig
 from scripts.configs.font import FontConfig
 
 font_version = '2024.05.12'
-
-font_version_time = datetime.datetime.fromisoformat(f'{font_version.replace('.', '-')}T00:00:00Z')
 
 font_sizes = [10, 12, 16]
 
--- scripts/services/font_service.py
+++ scripts/services/font_service.py
@@ -1,3 +1,4 @@
+import datetime
 import logging
 import math
 import re
@@ -253,8 +254,8 @@ def _create_builder(
     builder.font_metric.cap_height = layout_param.cap_height
 
     builder.meta_info.version = configs.font_version
-    builder.meta_info.created_time = configs.font_version_time
-    builder.meta_info.modified_time = configs.font_version_time
+    builder.meta_info.created_time = datetime.datetime.fromisoformat(f'{configs.font_version.replace('.', '-')}T00:00:00Z')
+    builder.meta_info.modified_time = builder.meta_info.created_time
     builder.meta_info.family_name = f'Ark Pixel {design_context.font_config.font_size}px {width_mode.capitalize()} {language_flavor}'
     builder.meta_info.weight_name = WeightName.REGULAR
     builder.meta_info.serif_style = SerifStyle.SANS_SERIF
