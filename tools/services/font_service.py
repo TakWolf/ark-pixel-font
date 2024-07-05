7a2fcc1e07da6ad2da056caf4e497f6ef655ab30
TakWolf <takwolf@foxmail.com>
2024-07-05 16:46:58 +0800

    调整 glyph_name 生成策略

--- tools/services/font_service.py
+++ tools/services/font_service.py
@@ -62,12 +62,12 @@ class GlyphFile:
     @property
     def glyph_name(self) -> str:
         if self.code_point == -1:
-            _glyph_name = '.notdef'
+            name = '.notdef'
         else:
-            _glyph_name = f'{self.code_point:04X}'
+            name = f'{self.code_point:04X}'
         if len(self.language_flavors) > 0:
-            _glyph_name = f'{_glyph_name}-{''.join([str(configs.language_flavors.index(language_flavor)) for language_flavor in self.language_flavors])}'
-        return _glyph_name
+            name = f'{name}-{self.language_flavors[0]}'
+        return name
 
 
 class DesignContext:
