6025a6c7df0409f781c437283639a5aaf57d330b
TakWolf <takwolf@foxmail.com>
2024-12-11 18:08:14 +0800

    调整 mapping 文件目录

--- tools/services/format_service.py
+++ tools/services/format_service.py
@@ -1,5 +1,6 @@
 import itertools
 import shutil
+from io import StringIO
 from pathlib import Path
 
 from pixel_font_knife import glyph_file_util, glyph_mapping_util
@@ -30,4 +31,27 @@ def format_glyphs(font_config: FontConfig):
 
 def format_mapping(file_path: Path):
     mapping = glyph_mapping_util.load_mapping(file_path)
-    glyph_mapping_util.save_mapping(mapping, file_path, configs.language_flavors)
+
+    only_default = True
+    for source_group in mapping.values():
+        if len(source_group) > 1 or None not in source_group:
+            only_default = False
+            break
+
+    if only_default:
+        buffer = StringIO()
+        for code_point, source_group in sorted(mapping.items()):
+            c = chr(code_point)
+            if not c.isprintable():
+                c = f'0x{code_point:04X}'
+            source_code_point = source_group[None].code_point
+            source_c = chr(source_code_point)
+            if not source_c.isprintable():
+                source_c = f'0x{source_code_point:04X}'
+            buffer.write('\n')
+            buffer.write(f'# {c} <- {source_c}\n')
+            buffer.write(f'0x{code_point:04X}:\n')
+            buffer.write(f'  ~: 0x{source_code_point:04X}\n')
+        file_path.write_text(buffer.getvalue(), 'utf-8')
+    else:
+        glyph_mapping_util.save_mapping(mapping, file_path, configs.language_flavors)
