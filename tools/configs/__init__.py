0deb5f5f863c68b768f118b2e2766f99af95fc95
TakWolf <takwolf@foxmail.com>
2025-01-06 04:51:52 +0800

    2025.01.06

--- tools/configs/__init__.py
+++ tools/configs/__init__.py
@@ -2,7 +2,7 @@ from typing import Literal, get_args
 
 from tools.configs import path_define
 
-version = '2024.11.04'
+version = '2025.01.06'
 
 type FontSize = Literal[10, 12, 16]
 font_sizes = list[FontSize](get_args(FontSize.__value__))
