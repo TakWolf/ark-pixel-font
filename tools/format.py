6b8df7fed133888a9664f2e53737a760d9e9794b
TakWolf <takwolf@foxmail.com>
2025-06-19 16:18:51 +0800

    优化格式化工具

--- tools/format.py
+++ tools/format.py
@@ -8,6 +8,9 @@ def main():
         font_config = FontConfig.load(font_size)
         format_service.format_glyphs(font_config)
 
+    for file_path in configs.mapping_file_paths:
+        format_service.format_mapping(file_path)
+
 
 if __name__ == '__main__':
     main()
