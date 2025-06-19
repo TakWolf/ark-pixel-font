cd05d3be8146681d0b6f4121a4db0ba16c78f12b
TakWolf <takwolf@foxmail.com>
2025-06-19 16:18:51 +0800

    优化格式化工具

--- tools/services/format_service.py
+++ tools/services/format_service.py
@@ -1,5 +1,4 @@
 import itertools
-import shutil
 from pathlib import Path
 
 from pixel_font_knife import glyph_file_util, glyph_mapping_util
@@ -9,23 +8,11 @@ from tools.configs import path_define
 from tools.configs.font import FontConfig
 
 
-def _is_empty_dir(path: Path) -> bool:
-    for item_path in path.iterdir():
-        if item_path.name == '.DS_Store':
-            continue
-        return False
-    return True
-
-
 def format_glyphs(font_config: FontConfig):
     for width_mode_dir_name in itertools.chain(['common'], configs.width_modes):
         width_mode_dir = path_define.glyphs_dir.joinpath(str(font_config.font_size), width_mode_dir_name)
         context = glyph_file_util.load_context(width_mode_dir)
         glyph_file_util.normalize_context(context, width_mode_dir, configs.language_flavors)
-
-        for file_dir, _, _ in width_mode_dir.walk(top_down=False):
-            if _is_empty_dir(file_dir):
-                shutil.rmtree(file_dir)
 
 
 def format_mapping(file_path: Path):
