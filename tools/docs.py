d907b5b285d5bd1cd00da606ae50a5fcb73f6d23
TakWolf <takwolf@foxmail.com>
2025-07-03 17:46:31 +0800

    调整 docs 命令

--- tools/docs.py
+++ tools/docs.py
@@ -1,12 +1,7 @@
-from tools import cli
 from tools.services import publish_service
 
 
 def main():
-    cli.main(
-        font_formats=set(),
-        attachments={'info', 'image'},
-    )
     publish_service.update_docs()
 
 
