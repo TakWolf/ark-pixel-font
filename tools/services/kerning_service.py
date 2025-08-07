2bab23d4f7960b51936818da4cab7f236e753a7b
TakWolf <takwolf@foxmail.com>
2025-08-07 22:32:40 +0800

    update

---
+++ tools/services/kerning_service.py
@@ -0,0 +1,9 @@
+from pixel_font_knife.glyph_file_util import GlyphFlavorGroup
+
+
+def calculate_kerning_pairs(context: dict[int, GlyphFlavorGroup]) -> dict[tuple[str, str], int]:
+    latin_code_points = set()
+
+    kerning_pairs = {}
+
+    return kerning_pairs
