b7e50f57edc62c93474aa4c77e0b7b8c09bf2bb7
TakWolf <takwolf@foxmail.com>
2025-09-28 02:59:12 +0800

    添加映射文件 1F100-1F1FF Enclosed Alphanumeric Supplement

--- tools/configs/__init__.py
+++ tools/configs/__init__.py
@@ -9,6 +9,7 @@ mapping_file_paths = [
     path_define.mappings_dir.joinpath('2700-27BF Dingbats.yml'),
     path_define.mappings_dir.joinpath('2E80-2EFF CJK Radicals Supplement.yml'),
     path_define.mappings_dir.joinpath('2F00-2FDF Kangxi Radicals.yml'),
+    path_define.mappings_dir.joinpath('1F100-1F1FF Enclosed Alphanumeric Supplement.yml'),
 ]
 
 locale_to_language_flavor = {
