96246542177ca833bd2efab7cfebcc79500ad5b2
TakWolf <takwolf@foxmail.com>
2025-12-07 01:36:16 +0800

    兼容旧 Windows 程序对等宽模式的识别问题

--- tools/services/font_service.py
+++ tools/services/font_service.py
@@ -141,6 +141,10 @@ class DesignContext:
         builder.opentype_config.fields_override.head_y_max = layout_metric.ascent
         builder.opentype_config.fields_override.head_y_min = layout_metric.descent
 
+        if width_mode == 'monospaced':
+            builder.opentype_config.is_monospaced = True
+            builder.opentype_config.fields_override.os2_x_avg_char_width = self.font_size // 2
+
         return builder
 
     def _create_collection_builder(self, width_mode: WidthMode) -> FontCollectionBuilder:
