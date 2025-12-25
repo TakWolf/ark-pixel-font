5fbc37139d64b1a99661d7b6c26a8439fbde6154
TakWolf <takwolf@foxmail.com>
2025-12-25 20:18:18 +0800

    代码整理

--- tools/services/format_service.py
+++ tools/services/format_service.py
@@ -17,6 +17,5 @@ def format_mappings():
     for file_path in path_define.mappings_dir.iterdir():
         if file_path.suffix != '.yml':
             continue
-
         mapping = glyph_mapping_util.load_mapping(file_path)
         glyph_mapping_util.save_mapping(mapping, file_path, options.language_flavors)
