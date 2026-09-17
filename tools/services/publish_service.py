09212f2e531aec299e4ce58961a16fb8b8d6c9e2
TakWolf <takwolf@foxmail.com>
2026-09-17 23:57:35 +0800

    调整变量名

--- tools/services/publish_service.py
+++ tools/services/publish_service.py
@@ -13,14 +13,14 @@ def make_release_zips(font_size: FontSize, width_mode: WidthMode, font_formats: 
     path_define.RELEASES_DIR.mkdir(parents=True, exist_ok=True)
 
     for font_format in font_formats:
-        file_path = path_define.RELEASES_DIR.joinpath(f'ark-pixel-font-{font_size}px-{width_mode}-{font_format}-v{configs.VERSION}.zip')
-        with ZipFile(file_path, 'w') as file:
+        zip_file_path = path_define.RELEASES_DIR.joinpath(f'ark-pixel-font-{font_size}px-{width_mode}-{font_format}-v{configs.VERSION}.zip')
+        with ZipFile(zip_file_path, 'w') as file:
             file.write(path_define.PROJECT_ROOT_DIR.joinpath('LICENSE-OFL'), 'OFL.txt')
 
             for language_flavor in options.LANGUAGE_FLAVORS:
-                font_file_name = f'ark-pixel-{font_size}px-{width_mode}-{language_flavor}.{font_format}'
-                file.write(path_define.OUTPUTS_DIR.joinpath(font_file_name), font_file_name)
-        logger.info("Make release zip: '{}'", file_path)
+                font_file_path = path_define.OUTPUTS_DIR.joinpath(f'ark-pixel-{font_size}px-{width_mode}-{language_flavor}.{font_format}')
+                file.write(font_file_path, font_file_path.name)
+        logger.info("Make release zip: '{}'", zip_file_path)
 
 
 def update_docs() -> None:
