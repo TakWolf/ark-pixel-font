d4cc14efc6acf8b58f6bece7a426d042c9b4e855
TakWolf <takwolf@foxmail.com>
2026-09-17 23:57:35 +0800

    代码整理

--- tools/services/publish_service.py
+++ tools/services/publish_service.py
@@ -30,5 +30,6 @@ def update_docs() -> None:
     for path_from in path_define.OUTPUTS_DIR.iterdir():
         if regex_file_name.match(path_from.name) is None and path_from.name != 'readme-banner.png':
             continue
+
         path_to = path_from.copy_into(path_define.DOCS_DIR)
         logger.info("Copy file: '{}' -> '{}'", path_from, path_to)
