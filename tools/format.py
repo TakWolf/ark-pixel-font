9c7c7086f774b1e425932ae8aef8c9166cb7b9bf
TakWolf <takwolf@foxmail.com>
2026-09-17 21:04:18 +0800

    修复 format 工作流

--- tools/format.py
+++ tools/format.py
@@ -4,8 +4,9 @@ from tools.services import format_service
 
 def main() -> None:
     for font_size in options.FONT_SIZES:
-        format_service.format_glyphs(font_size)
+        format_service.normalize_cmap_glyphs(font_size)
 
+    format_service.format_glyphs()
     format_service.format_mappings()
 
 
--- tools/services/format_service.py
+++ tools/services/format_service.py
@@ -1,22 +1,29 @@
-from pixel_font_knife import glyph_file_util, glyph_mapping_util
-from pixel_font_knife.glyph_file_util import GlyphFile
+import sys
+
+from pixel_font_knife.cmap.context import CmapContext
+from pixel_font_knife.cmap.mapping.mapping import CmapMapping
+from pixel_font_knife.utils import fs_util
 
 from tools.configs import path_define, options
 from tools.configs.options import FontSize
 
 
-def format_glyphs(font_size: FontSize) -> None:
-    GlyphFile.load(path_define.GLYPHS_DIR.joinpath(str(font_size), 'notdef.png')).save()
-
+def normalize_cmap_glyphs(font_size: FontSize) -> None:
     for glyph_scope in options.GLYPH_SCOPES:
         glyph_scope_dir = path_define.GLYPHS_DIR.joinpath(str(font_size), 'cmap', glyph_scope)
-        context = glyph_file_util.load_context(glyph_scope_dir)
-        glyph_file_util.normalize_context(context, glyph_scope_dir, options.LANGUAGE_FLAVORS)
+        context = CmapContext.load(glyph_scope_dir, allowed_flavors=options.LANGUAGE_FLAVORS)
+        context.normalize(glyph_scope_dir, flavor_order=options.LANGUAGE_FLAVORS)
+
+
+def format_glyphs() -> None:
+    if sys.platform != 'win32':
+        fs_util.format_glyph_files(path_define.GLYPHS_DIR)
 
 
 def format_mappings() -> None:
     for file_path in path_define.CONFIGS_MAPPINGS_DIR.iterdir():
         if file_path.suffix != '.yaml':
             continue
-        mapping = glyph_mapping_util.load_mapping(file_path)
-        glyph_mapping_util.save_mapping(mapping, file_path, options.LANGUAGE_FLAVORS)
+
+        mapping = CmapMapping.load_yaml(file_path, allowed_flavors=options.LANGUAGE_FLAVORS)
+        mapping.save_yaml(file_path, flavor_order=options.LANGUAGE_FLAVORS)
